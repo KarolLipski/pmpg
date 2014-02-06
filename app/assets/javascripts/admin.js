7834b8fe4ce06cae7261c5a439fc91976f3dcef4
karol.lipski@gmail.com <karol.lipski@gmail.com>
2014-02-06 20:13:56 +0100

    charisma layout

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,49 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require ../charisma/js/jquery-1.7.2.min.js
+//= require jquery_ujs
+//= require ../charisma/js/jquery-ui-1.8.21.custom.min.js
+//= require ../charisma/js/bootstrap-transition.js
+//= require ../charisma/js/bootstrap-alert.js
+//= require ../charisma/js/bootstrap-modal.js
+//= require ../charisma/js/bootstrap-dropdown.js
+//= require ../charisma/js/bootstrap-scrollspy.js
+//= require ../charisma/js/bootstrap-tab.js
+//= require ../charisma/js/bootstrap-tooltip.js
+//= require ../charisma/js/bootstrap-popover.js
+//= require ../charisma/js/bootstrap-button.js
+//= require ../charisma/js/bootstrap-collapse.js
+//= require ../charisma/js/bootstrap-carousel.js
+//= require ../charisma/js/bootstrap-typeahead.js
+//= require ../charisma/js/bootstrap-tour.js
+//= require ../charisma/js/jquery.cookie.js
+//= require ../charisma/js/fullcalendar.min.js
+//= require ../charisma/js/jquery.dataTables.min.js
+//= require ../charisma/js/excanvas.js
+//= require ../charisma/js/jquery.flot.min.js
+//= require ../charisma/js/jquery.flot.pie.min.js
+//= require ../charisma/js/jquery.flot.stack.js
+//= require ../charisma/js/jquery.flot.resize.min.js
+//= require ../charisma/js/jquery.chosen.min.js
+//= require ../charisma/js/jquery.uniform.min.js
+//= require ../charisma/js/jquery.colorbox.min.js
+//= require ../charisma/js/jquery.cleditor.min.js
+//= require ../charisma/js/jquery.noty.js
+//= require ../charisma/js/jquery.elfinder.min.js
+//= require ../charisma/js/jquery.raty.min.js
+//= require ../charisma/js/jquery.iphone.toggle.js
+//= require ../charisma/js/jquery.autogrow-textarea.js
+//= require ../charisma/js/jquery.uploadify-3.1.min.js
+//= require ../charisma/js/jquery.history.js
+//= require ../charisma/js/charisma.js
+

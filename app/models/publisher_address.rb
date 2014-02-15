1d6a4749d354d39beabbf257d4ed32981d312882
karol.lipski@gmail.com <karol.lipski@gmail.com>
2014-02-15 19:39:53 +0100

    validatin nip and postal code

--- app/models/publisher_address.rb
+++ app/models/publisher_address.rb
@@ -6,12 +6,12 @@ class PublisherAddress < ActiveRecord::Base
   validates_presence_of :company_name, :street, :street_no, :postal_code,
     :city, :address_type, :publisher
   validates_presence_of :nip ,if: "address_type == 'invoice'"
-  validates_format_of :postal_code, with: /\d{2}-\d{3}\z/
+  validates_format_of :postal_code, with: /\A\d{2}-\d{3}\z/
   validate :nip_format,if: "address_type == 'invoice'"
 
   def nip_format
     return unless nip
-    errors.add(:nip, "has wrong format") unless nip.gsub('-','') =~ /\d{10}\z/
+    errors.add(:nip, "has wrong format") unless nip.gsub('-','') =~ /\A\d{10}\z/
   end
 
 end

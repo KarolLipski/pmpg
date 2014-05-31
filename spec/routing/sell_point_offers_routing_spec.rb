e2dbd63cde010ade434822f52ae7d8f306bc2ea3
karol.lipski@gmail.com <karol.lipski@gmail.com>
2014-05-31 23:07:44 +0200

    routes spec pass

--- spec/routing/sell_point_offers_routing_spec.rb
+++ spec/routing/sell_point_offers_routing_spec.rb
@@ -4,31 +4,31 @@ describe SellPointOffersController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/sell_point_offers").should route_to("sell_point_offers#index")
+      get("/sell_points/1/offers").should route_to("sell_point_offers#index", sell_point_id: "1")
     end
 
     it "routes to #new" do
-      get("/sell_point_offers/new").should route_to("sell_point_offers#new")
+      get("/sell_points/1/offers/new").should route_to("sell_point_offers#new",sell_point_id: "1")
     end
 
     it "routes to #show" do
-      get("/sell_point_offers/1").should route_to("sell_point_offers#show", :id => "1")
+      get("/sell_points/1/offers/1").should route_to("sell_point_offers#show",sell_point_id: "1", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/sell_point_offers/1/edit").should route_to("sell_point_offers#edit", :id => "1")
+      get("/sell_points/1/offers/1/edit").should route_to("sell_point_offers#edit",sell_point_id: "1", :id => "1")
     end
 
     it "routes to #create" do
-      post("/sell_point_offers").should route_to("sell_point_offers#create")
+      post("/sell_points/1/offers").should route_to("sell_point_offers#create",sell_point_id: "1")
     end
 
     it "routes to #update" do
-      put("/sell_point_offers/1").should route_to("sell_point_offers#update", :id => "1")
+      put("/sell_points/1/offers/1").should route_to("sell_point_offers#update",sell_point_id: "1", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/sell_point_offers/1").should route_to("sell_point_offers#destroy", :id => "1")
+      delete("/sell_points/1/offers/1").should route_to("sell_point_offers#destroy",sell_point_id: "1", :id => "1")
     end
 
   end
